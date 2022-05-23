032a22e2f28d75faaf7a503606497d92814dddc4
OmerYesilkaya <omer.yesilkaya@seddi.com>
2022-05-23 23:42:28 +0300

    first commit (setting up the project)

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                default: '"Lexend Deca", sans-serif',
+                code: '"Ubuntu Mono", sans-serif',
+                title: '"Questrial", sans-serif',
+            },
+        },
+    },
+    plugins: [],
+};
