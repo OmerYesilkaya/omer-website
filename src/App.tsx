f3cf3ad861c2dec28d575e282099fbde9a1b2699
OmerYesilkaya <omer.yesilkaya@seddi.com>
2022-05-23 23:50:02 +0300

    add title to homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 function App() {
-    return <div className="h-screen w-screen flex items-center justify-center">test</div>;
+    return (
+        <div className="h-screen w-screen flex items-center justify-center">
+            <div className="rounded-lg border-2 border-amber-500 w-1/2 h-1/3 p-10 flex flex-col gap-y-4">
+                <h2 className="font-title text-7xl">OMER YESILKAYA</h2>
+                <span className="">Hello, this is my website.</span>
+            </div>
+        </div>
+    );
 }
 
 export default App;
